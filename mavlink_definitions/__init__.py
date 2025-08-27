a503758c2c56bdab3061c37c1aae0d7a2f0afe76
Mykola Balakin <mykola@balakin.me>
2025-08-27 05:35:13 +0300

    Allow choosing the MAVLink definitions file

--- mavlink_definitions/__init__.py
+++ mavlink_definitions/__init__.py
@@ -1,4 +1,11 @@
 from .definitions import MAVLinkDefinitions
 from .types import MAVLinkEnum, MAVLinkMessage, MAVLinkMessageField
+from .parser import get_all_mavlink_definitions
 
-__all__ = ["MAVLinkDefinitions", "MAVLinkEnum", "MAVLinkMessage", "MAVLinkMessageField"]
+__all__ = [
+    "MAVLinkDefinitions",
+    "MAVLinkEnum",
+    "MAVLinkMessage",
+    "MAVLinkMessageField",
+    "get_all_mavlink_definitions",
+]
